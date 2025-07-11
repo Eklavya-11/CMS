2237a44de244a31d88ae8ae52560dcf14a78d0b2
Eklavya11 <117552685+Eklavya-11@users.noreply.github.com>
2025-07-11 22:48:04 +0530

    Create views.py

---
+++ users/views.py
@@ -0,0 +1,19 @@
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_register_view(request):
+    # Logic for the login view
+    return render(request, 'users/login_register.html')
